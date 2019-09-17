3f548b632ec5b25f642b28cc230e292755bedf3a
Thiago Cortes <thiago.cortes@datasprints.com>
2019-09-17 10:42:14 -0300

    Add yandeh test

--- models/example/produtos_name_qtde.sql
+++ models/example/produtos_name_qtde.sql
@@ -4,7 +4,10 @@
 -- the SQL file? This will override configurations stated in dbt_project.yml
 
 -- Try changing 'view' to 'table', then re-running dbt
-{{ config(materialized='view') }}
+{{ config(materialized='table') }}
 
 
-select distinct * from public."cliente_raw"
+select name,count(*) from recomendacao_app."produtos"
+group by name
+order by 2 desc
+limit 10;
