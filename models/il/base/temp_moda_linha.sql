969e1b47d1cdc8692b7d616791b2c23c069e5c97
Thiago Cortes <thiago.cortes@datasprints.com>
2019-09-17 15:42:42 -0300

    Add yandeh test

--- models/il/base/temp_moda_linha.sql
+++ models/il/base/temp_moda_linha.sql
@@ -5,7 +5,8 @@ SELECT
     SUM(Qtd_item),
     RANK() OVER (PARTITION BY cnpj_loja, id_linha ORDER BY SUM(Qtd_item) DESC,(fat_item/Qtd_item) :: numeric(18, 2) DESC ) AS Ordem_moda
 FROM
-   {{ref('temp_base_sellout_linha')}}
+   --{{ref('temp_base_sellout_linha')}}
+   temp_base_sellout_linha
 WHERE
     TRUE
 GROUP BY
