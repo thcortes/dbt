867d183ce0baf129fef9c56946207af650cacf15
Thiago Cortes <thiago.cortes@datasprints.com>
2019-09-24 16:50:29 -0300

    Add target.name

--- models/il_v2/base_sellout_v2_max.sql
+++ models/il_v2/base_sellout_v2_max.sql
@@ -3,9 +3,9 @@
 SELECT
     A.cnpj_loja,
     A.Sku,
-    max(A.tkt_item),
-    max(A.fat_item),
-    max(A.Qtd_item),
+    max(A.tkt_item) as tkt_item,
+    max(A.fat_item) as fat_item,
+    max(A.Qtd_item) as Qtd_item,
     max(((A.fat_item) /(A.Qtd_item)) :: numeric(18, 2)) AS Preco
 FROM
     {{ref(var('temp_base_sellout2'))}}  A
--- models/il_v2/base_sellout_v2_min.sql
+++ models/il_v2/base_sellout_v2_min.sql
@@ -3,9 +3,9 @@
 SELECT
     A.cnpj_loja,
     A.Sku,
-    min(A.tkt_item),
-    min(A.fat_item),
-    min(A.Qtd_item),
+    min(A.tkt_item)as tkt_item,
+    min(A.fat_item) as fat_item,
+    min(A.Qtd_item) as Qtd_item,
     min(((A.fat_item) /(A.Qtd_item)) :: numeric(18, 2)) AS Preco
 FROM
     {{ref(var('temp_base_sellout2'))}}  A
